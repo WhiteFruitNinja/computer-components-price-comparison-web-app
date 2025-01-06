055965f0b93ba2a4da176d27a8ba5cdfc619f859
WhiteFruitNinja <whitefruitninja@gmail.com>
2025-01-06 16:35:12 +0200

    Initial commit

---
+++ src/main/java/com/example/ComputerComponentsPriceComparisonWebApp/ComputerComponentsPriceComparisonWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ComputerComponentsPriceComparisonWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerComponentsPriceComparisonWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerComponentsPriceComparisonWebAppApplication.class, args);
+	}
+
+}
